2d6d9c89bec720ad29cd16690e57ca0d9fb07975
jcardonne <contact@jcardonne.com>
2021-10-27 14:56:35 +0200

    UPDATE: Condition Functions to compare strings

--- condition.c
+++ condition.c
@@ -7,10 +7,50 @@
 
 int EQUAL(char *a, char *b)
 {
-    char *str1 = clean_sign(a);
-    char *str2 = clean_sign(b);
+    char *str1 = absolute_cleaning(a);
+    char *str2 = absolute_cleaning(b);
 
     if (my_strcmp(str1, str2) == 0)
+        return (1);
+    return (0);
+}
+
+int GREATER_EQUALS(char *a, char *b)
+{
+    char *str1 = absolute_cleaning(a);
+    char *str2 = absolute_cleaning(b);
+
+    if (my_strcmp(str1, str2) >= 0)
+        return (1);
+    return (0);
+}
+
+int GREATER(char *a, char *b)
+{
+    char *str1 = absolute_cleaning(a);
+    char *str2 = absolute_cleaning(b);
+
+    if (my_strcmp(str1, str2) >= 1)
+        return (1);
+    return (0);
+}
+
+int LOWER(char *a, char *b)
+{
+    char *str1 = absolute_cleaning(a);
+    char *str2 = absolute_cleaning(b);
+
+    if (my_strcmp(str1, str2) <= -1)
+        return (1);
+    return (0);
+}
+
+int LOWER_EQUALS(char *a, char *b)
+{
+    char *str1 = absolute_cleaning(a);
+    char *str2 = absolute_cleaning(b);
+
+    if (my_strcmp(str1, str2) <= 0)
         return (1);
     return (0);
 }
