bf78cd9e3b52fb345e253eb3a63c0125d17413b0
jcardonne <contact@jcardonne.com>
2021-11-04 18:22:00 +0100

    UPDATE: Infindiv rewrite (need to verify coding style)

--- src/operators/div_mod/infin_div.c
+++ src/operators/div_mod/infin_div.c
@@ -1,18 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "my_operators.h"
 
 char *my_arr_sum(char **array)
 {
     int count = 0;
-    int array_count = 0;
-    char result = NULL;
+    char *result = NULL;
+    int i;
+    int index = 0;
 
-    for (; array[array_count] != 0; array_count++)
-        count += my_strlen(array[array_count]);
+    for (i = 0; array[i] != 0; i++)
+        count += my_strlen(array[i]);
     result = malloc(sizeof(char) * (count + 2));
-    for (int i = 0; i < array_count; i++)
-        result = infin_add(result, array[i]);
+    result = "0";
+    index = i;
+    for (i = 0; i < index; i++)
+        result = my_add(result, array[i]);
+    result[count] = '\0';
     return (result);
+}
+
+char *my_str_sum(char *input)
+{
+    int count = 0;
+    int array_count = 0;
+
+    for (int i = 0; input[i] != '\0'; i++)
+        count += input[i] - '0';
+    return (my_getnbr(count));
 }
 
 char *infin_div(char *dividend, char *divisor, int modulo)
@@ -20,39 +35,37 @@ char *infin_div(char *dividend, char *divisor, int modulo)
     char *temp_value = malloc(sizeof(char) * my_strlen(divisor));
     char ***table = malloc(sizeof(char**) * 100);
     int done = 0;
-    int temp_key = 1;
+    char *temp_key = "1";
     int index_table = 0;
 
     //FILLING LE TABLEAU TQT comme sur wiki
     while (done == 0) {
-        temp_value = infin_mul(temp_key, divisor);
+        temp_value = my_mul(temp_key, divisor);
         if (my_greater_equals(temp_value, dividend)) {
             done = 1;
             break;
         }
-        table[0][index_table] = temp_key;
-        table[1][index_table] = temp_value;
+        char **lines = malloc(sizeof(char*) * 3); // 2 place and one for '\0'
+        lines[0] = temp_key;
+        lines[1] = temp_value;
+        table[index_table] = lines; // ERRROR
         index_table++;
-        temp_key *= 2;
+        temp_key = my_mul(temp_key, "2");
     }
 
     // COMPARAISON DU TABLEAU
-    char *decision = malloc(sizeof(char) * my_strlen(divisor));
-    char **decision_table = malloc(sizeof(char*) * 100);
-    int index_decision = 0;
+    char **values = malloc(sizeof(char*) * (index_table + 1));
+    int value = 1;
+    char *result = malloc(sizeof(char) * my_strlen(dividend));
 
-    for (int i = index_table; i > 0; i--) {
-        if (i == index_table && my_equal(table[1][i], dividend)) {
-            decision = infin_add(decision, table[1][i]);
-            decision_table[index_decision++] = table[1][i];
-            break;
-        } else if (my_lower_equals(ADD(my_arr_sum(decision_table), table[1][i])), dividend) {
-            decision = infin_add(decision, table[1][i]);
-            decision_table[index_decision++] = table[1][i];
-        } else if (UPPER(ADD(my_arr_sum(decision_table), table[1][i])), dividend) {
-            // do nothing
+    values[0] = table[index_table - 1][1];
+    result = my_add(result, table[index_table - 1][0]);
+    for (int i = index_table - 1; i >= 0; i--) {
+        if (my_lower(my_add(my_arr_sum(values), table[i][1]), dividend)) {
+            values[value] = table[i][1];
+            value++;
+            result = my_add(result, table[i][0]);
         }
     }
-
-    return modulo ? (decision) : (my_arr_sum(decision_table));
+    return modulo ? (my_sub(dividend, my_arr_sum(values))) : (result);
 }
