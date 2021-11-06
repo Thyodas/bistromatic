3c1ad0be0ad1731b4f20d60201ea1e9fb08a4717
Thyodas <guillaume.hein@epitech.eu>
2021-11-06 19:47:07 +0100

    Fixed huge flaw in op_handle in shunting yard, op list was shifted

--- src/eval_expr/shunting_yard.c
+++ src/eval_expr/shunting_yard.c
@@ -23,8 +23,8 @@ operator_t *operators)
     int op_weight = 0;
 
     if (op->weight != 0) {
-        op_weight = get_op(op, op_str)->weight;
-        while (get_op(op, top)->weight >= op_weight && my_strlen(top) != 0) {
+        op_weight = get_op(operators, op_str)->weight;
+        while (get_op(operators, top)->weight >= op_weight && my_strlen(top) != 0) {
             queue_add(queue, stack_pop(stack));
             top = stack_peek(stack);
         }
