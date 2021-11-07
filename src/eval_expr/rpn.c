f07eae9f9f6b2ac5ed997c21b12b307897cf52c2
iMeaNz <mehdy.morvan@epitech.eu>
2021-11-07 13:33:09 +0100

    Removed useless function

--- src/eval_expr/rpn.c
+++ src/eval_expr/rpn.c
@@ -15,30 +15,6 @@
 
 char *clean_zero_before(char *input);
 
-char *int_to_str(int nb)
-{
-    int i = 0;
-    int neg = 0;
-    char *str = malloc(sizeof(char) * 12);
-
-    if (nb < 0) {
-        neg = 1;
-        nb *= -1;
-    }
-    for (; nb >= 0; i++) {
-        str[i] = (nb % 10) + '0';
-        nb /= 10;
-        if (nb == 0)
-            break;
-    }
-    if (neg == 1) {
-        str[i + 1] = '-';
-        i++;
-    }
-    str[i + 1] = '\0';
-    return (my_revstr(str));
-}
-
 char *calculate_previous(stack_t *stack, operator_t *op)
 {
     char *res = NULL;
