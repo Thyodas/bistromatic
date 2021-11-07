5d01abf58718dc118930b79efaff52f5b7bebb20
iMeaNz <mehdy.morvan@epitech.eu>
2021-11-07 18:48:21 +0100

    iMeaNz : Improved coverage

--- src/utilities/condition.c
+++ src/utilities/condition.c
@@ -18,17 +18,6 @@ int my_equal(char *a, char *b)
     return (0);
 }
 
-int my_greater_equals(char *a, char *b)
-{
-    char *str1 = a;
-    char *str2 = b;
-    int cmp = cmp_gt(str1, str2);
-
-    if (cmp >= 0)
-        return (1);
-    return (0);
-}
-
 int my_greater(char *a, char *b)
 {
     char *str1 = a;
