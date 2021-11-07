9da7364741c055e715ba4708bbba42757e34439d
iMeaNz <mehdy.morvan@epitech.eu>
2021-11-07 08:52:12 +0100

    iMeaNz : decimal_to_base now handles negatives

--- src/utilities/decimal_to_base.c
+++ src/utilities/decimal_to_base.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 
 int stack_base_size(stack_base *begin);
+int find_sign(char *number, char *operators, int *nb_of_sign);
 
 char *compute_stack(stack_base *stack, char *base_to)
 {
@@ -18,41 +19,57 @@ char *compute_stack(stack_base *stack, char *base_to)
     int index = 0;
 
     while (stack != NULL) {
-        char mod = my_getnbr(stack->nbr);
-        free(stack->nbr);
-        char to_push = base_to[mod];
-        output[index++] = to_push;
+        if (stack->nbr[0] == '-')
+            output[index++] = '-';
+        else {
+            int mod = my_getnbr(stack->nbr);
+            free(stack->nbr);
+            char to_push = base_to[mod];
+            output[index++] = to_push;
+        }
+        stack = stack->next;
     }
     output[index] = '\0';
     return output;
 }
 
-stack_base *fill_stack_nbr(char *nbr, char *base)
+char *push_in_stack(stack_base **stack, char *nb, char *len_base_in_str)
+{
+    char *remainder = my_mod(nb, len_base_in_str);
+    char *to_push = clean_zero_before(remainder);
+    my_add_in_stack_base(stack, to_push);
+    int len_nb = my_strlen(nb);
+    char *tmp = malloc(sizeof(char) * len_nb + 1);
+    my_strcpy(tmp, nb);
+    nb = my_div(tmp, len_base_in_str);
+    free(tmp);
+    return nb;
+}
+
+stack_base *fill_stack_nbr(char *nbr, char *base, char *operators)
 {
     stack_base *stack = NULL;
+    int nb_of_sign = 0;
     int len_base = my_strlen(base);
     int size = size_nbr(len_base);
     char *len_base_in_str = malloc(sizeof(char) * size + 1);
     str_initialize_alloc(len_base_in_str, size + 1);
     my_putnbr_base_str(len_base, "0123456789", len_base_in_str);
+    int sign = find_sign(nbr, operators, &nb_of_sign);
+    nbr = &nbr[nb_of_sign];
 
     while (nbr[0] != '0') {
-        char *remainder = my_mod(nbr, len_base_in_str);
-        char *to_push = clean_zero_before(remainder);
-        my_add_in_stack_base(&stack, to_push);
-        int len_nbr = my_strlen(nbr);
-        char *tmp = malloc(sizeof(char) * len_nbr + 1);
-        my_strcpy(tmp, nbr);
-        nbr = my_div(tmp, len_base_in_str);
-        free(tmp);
+        nbr = push_in_stack(&stack, nbr, len_base_in_str);
     }
     free(len_base_in_str);
+    if (sign == -1)
+        my_add_in_stack_base(&stack, my_strdup("-"));
     return stack;
 }
 
 char *decimal_to_base(char *nb, char *base_to, char *operators)
 {
-    stack_base *list = fill_stack_nbr(nb, base_to);
+    stack_base *list = fill_stack_nbr(nb, base_to, operators);
     char *output = compute_stack(list, base_to);
     return (output);
 }
